78624e722aa293b7a708febe9357d6300b4c210a
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-04 10:17:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
+require 'pry'
+def sort_array_asc (arr)
+  arr.sort do |val1, val2|
+    val1 <=> val2
+  end
+end
 
+def sort_array_desc (arr)
+  arr.sort do |val1, val2|
+    val2 <=> val1
+  end
+end
+
+def sort_array_char_count(arr_s)
+  arr_s.sort do |str1, str2|
+    str1.length <=> str2.length
+  end
+end
+
+def swap_elements(arr)
+  arr.each do |elem|
+      yield elem
+  end
+end
+
+swap_elements([]){
+  |i| 
+  i
+  binding.pry
+}
